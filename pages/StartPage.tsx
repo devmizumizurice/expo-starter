35121f366c39971228725cf441a6a9fdee3b7d88
みずまんま <app.mizumizurice@gmail.com>
2023-12-29 01:19:58 +0900

    fixed

--- pages/StartPage.tsx
+++ pages/StartPage.tsx
@@ -3,12 +3,12 @@ import { View } from 'react-native';
 import StartMessage from '../components/start/StartMessage';
 
 const StartPage = () => {
-  return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <StartMessage />
-      <StatusBar style='auto' />
-    </View>
-  );
+    return (
+        <View className='flex-1 items-center justify-center bg-white'>
+            <StartMessage />
+            <StatusBar style='auto' />
+        </View>
+    );
 };
 
 export default StartPage;
