7a6a5bb42917082d6ff35287cbf2811ea0e51f8b
みずまんま <app.mizumizurice@gmail.com>
2023-12-28 22:53:45 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended'
+    ],
+    'ignorePatterns': ['.eslintrc.js', 'tailwind.config.js', 'babel.config.js'],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint',
+        'react'
+    ],
+    'rules': {
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off'
+    }
+};
